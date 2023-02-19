29ea9d837ea40cacd853e366619d43505510c0e9
Andry <andry.peshko@gmail.com>
2023-02-19 19:25:13 +0300

    edit 1_4

--- 1_4.py
+++ 1_4.py
@@ -4,9 +4,7 @@ first_num = int(input('Enter first number: '))
 second_num = int(input('Enter second number: '))
 third_num = int(input('Enter third number: '))
 
-col_pos_number = ((first_num * first_num)**(1/2) + first_num) / (first_num*2) + \
-                      ((second_num * second_num) ** (1/2) + second_num) / (second_num*2) + \
-                      ((third_num * third_num) ** (1/2) + third_num) / (third_num*2)
-col_neg_number = 3 - col_pos_number
+col_pos_number = (first_num > 0) + (second_num > 0) + (third_num > 0)
+col_neg_number = (first_num < 0) + (second_num < 0) + (third_num < 0)
 
-print(f"Number of positive numbers: {round(col_pos_number)} \nNumber of negative numbers: {round(col_neg_number)}")
+print(f'Number of positive numbers: {col_pos_number} \nNumber of negative numbers: {col_neg_number}')
