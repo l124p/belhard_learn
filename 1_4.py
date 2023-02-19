2ebc6d2c45fb6f94f091cc3d8af5e2ef01759219
andry <andry.peshko@gmail.com>
2023-02-19 15:24:47 +0300

    edit 1_4

--- 1_4.py
+++ 1_4.py
@@ -1,10 +1,12 @@
 # Подсчёт положительных и отрицательных чисел.
 
-first_number = input('Enter first number: ')
-second_number = input('Enter second number: ')
-third_number = input('Enter third number: ')
+first_num = int(input('Enter first number: '))
+second_num = int(input('Enter second number: '))
+third_num = int(input('Enter third number: '))
 
-sum_negative_number = first_number.count('-')
-sum_positive_number = 3 - sum_negative_number
+col_pos_number = ((first_num * first_num)**(1/2) + first_num) / (first_num*2) + \
+                      ((second_num * second_num) ** (1/2) + second_num) / (second_num*2) + \
+                      ((third_num * third_num) ** (1/2) + third_num) / (third_num*2)
+col_neg_number = 3 - col_pos_number
 
-print(f"Number of positive numbers: {sum_positive_number} \nNumber of negative numbers: {sum_negative_number}")
+print(f"Number of positive numbers: {round(col_pos_number)} \nNumber of negative numbers: {round(col_neg_number)}")
