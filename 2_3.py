3d9c2edb9da8f5fe470993bf27562c5ea0cc6926
andry <andry.peshko@gmail.com>
2023-02-21 10:02:03 +0300

    add вариант 2

--- 2_3.py
+++ 2_3.py
@@ -1,12 +1,11 @@
 # Вывести четные числа от 2 до N по 5 в строку
 
 n = int(input())
+number = [i for i in range(2, n + 1) if not i % 2]
 
 # Вариант 1
-number = [i for i in range(2, n + 1) if not i % 2]
 for i in range(0, len(number), 5):
     print(*number[i:i + 5])
-
 
 # Вариант 2
 i = 0
