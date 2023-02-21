99d23a0bef3970cee8169fc98b4e4448f03f2476
andry <andry.peshko@gmail.com>
2023-02-21 11:45:56 +0300

    add вариант 3

--- 2_3.py
+++ 2_3.py
@@ -4,13 +4,26 @@ n = int(input())
 number = [i for i in range(2, n + 1) if not i % 2]
 
 # Вариант 1
+print("Вариант 1")
 for i in range(0, len(number), 5):
     print(*number[i:i + 5])
 
 # Вариант 2
+print("Вариант 2")
 i = 0
 for i in range(len(number)):
     if (i + 1) % 5:
         print(number[i], end=' ')
     else:
         print(number[i])
+
+# Вариант 3
+print("\nВариант 3")
+i = 0
+j = 1
+for i in range(2, (n + 1), 2):
+    if j % 5:
+        print(i, end=' ')
+    else:
+        print(i)
+    j += 1
