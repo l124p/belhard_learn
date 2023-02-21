d3d84826401df31752a0998ac22d11a57ea9f462
andry <andry.peshko@gmail.com>
2023-02-21 09:59:20 +0300

    add вариант 2

--- 2_3.py
+++ 2_3.py
@@ -2,6 +2,16 @@
 
 n = int(input())
 
-number = [i for i in range(2,n+1) if not i % 2]
-for i in range(0,len(number),5):
-     print(*number[i:i+5])
+# Вариант 1
+number = [i for i in range(2, n + 1) if not i % 2]
+for i in range(0, len(number), 5):
+    print(*number[i:i + 5])
+
+
+# Вариант 2
+i = 0
+for i in range(len(number)):
+    if i % 5:
+        print(number[i], end=' ')
+    else:
+        print(number[i])
