e11b4061b924c29ffaf5bd9f4f521c789f4c7745
andry <andry.peshko@gmail.com>
2023-02-21 13:34:20 +0300

    edit 2_6

--- 2_6.py
+++ 2_6.py
@@ -1,5 +1,5 @@
 # Словарь из n вложенных словарей с элементами name и email
 
 n = int(input("number of elements: "))
-dic_clients = {i: {input("Enter name: "): input("Enter email: ")} for i in range(n)}
+dic_clients = {i: {'name': input("Enter name: "), 'email': input("Enter email: ")} for i in range(n)}
 print(dic_clients)
