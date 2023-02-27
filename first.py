c78e90711ba63474b0bd22bd161e52bd7f4162ab
= <gopochok228@gmail.com>
2023-02-27 17:01:21 +0300

    lesson 4

--- first.py
+++ first.py
@@ -15,39 +15,62 @@
 # print(ob.perimetr())
 # print(ob.square())
 
-class Category:
-    categories = ['A', 'B', 'C', 'CE', 'D', 'DE']
+# class Category:
+#     categories = ['A', 'B', 'C', 'CE', 'D', 'DE']
+#
+#     @classmethod
+#     def add(cls,names_category):
+#         if names_category not in cls.categories:
+#             cls.categories.append(names_category)
+#             return cls.categories.index(names_category)
+#         else:
+#             raise ValueError('category is not uniqui')
+#
+#     @classmethod
+#     def get(cls,index_category):
+#         try:
+#             return cls.categories[index_category]
+#         except:
+#             raise IndexError('error index')
+#
+#     @classmethod
+#     def delete(cls, index_category):
+#         try:
+#             cls.categories.remove(index_category)
+#         except:
+#             return None
+#
+#     @classmethod
+#     def update(cls, index_category, names_category):
+#         if names_category not in cls.categories:
+#             cls.categories[index_category] = names_category
+#         elif:
+#             return cls.add(names_category)
+#         else:
+#             raise ValueError('category is not uniqui')
+#
+#
+# print(Category.get(1))
 
-    @classmethod
-    def add(cls,names_category):
-        if names_category not in cls.categories:
-            cls.categories.append(names_category)
-            return cls.categories.index(names_category)
+
+class RegisterForm:
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def is_valid(self):
+        if not isinstance(self.username, str) or not isinstance(self.password, str):
+            return False
+        elif len(self.username) < 2 or len(self.password) < 8:
+            return False
+        elif self.username in self.password:
+            return False
+        elif self.password.isalpha():
+            return False
         else:
-            raise ValueError('category is not uniqui')
-
-    @classmethod
-    def get(cls,index_category):
-        try:
-            return cls.categories[index_category]
-        except:
-            raise IndexError('error index')
-
-    @classmethod
-    def delete(cls, index_category):
-        try:
-            cls.categories.remove(index_category)
-        except:
-            return None
-
-    @classmethod
-    def update(cls, index_category, names_category):
-        if names_category not in cls.categories:
-            cls.categories[index_category] = names_category
-        elif:
-            return cls.add(names_category)
-        else:
-            raise ValueError('category is not uniqui')
+            return True
 
 
-print(Category.get(1))
+my_ob = RegisterForm('name','password')
+print(my_ob.is_valid())
